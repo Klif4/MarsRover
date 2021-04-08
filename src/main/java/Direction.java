df06581871731f9f2f6eff023bc422525c2572b6
klif <klif.cheloul@gmail.com>
2021-04-08 20:37:06 +0200

    wip

--- src/main/java/Direction.java
+++ src/main/java/Direction.java
@@ -1,20 +1,31 @@
 public enum Direction {
-  EAST(Location::incrX),
-  NORTH(Location::decrY),
-  WEST(Location::decrX),
-  SOUTH(Location::incrY);
+  EAST(Location::incrX, Location::decrX),
+  NORTH(Location::decrY, Location::incrY),
+  WEST(Location::decrX, Location::incrX),
+  SOUTH(Location::incrY, Location::decrY);
 
   private final LocationForwarding locationForwarding;
+  private final LocationBackwarding locationBackwarding;
 
-  Direction(LocationForwarding test) {
-    this.locationForwarding = test;
+  Direction(LocationForwarding locationForwarding, LocationBackwarding locationBackwarding) {
+    this.locationForwarding = locationForwarding;
+    this.locationBackwarding = locationBackwarding;
   }
+
 
   public Location forwardLocation(Location location) {
     return locationForwarding.handle(location);
   }
 
+  public Location backwardLocation(Location location) {
+    return locationBackwarding.handle(location);
+  }
+
   interface LocationForwarding {
+    Location handle(Location location);
+  }
+
+  interface LocationBackwarding {
     Location handle(Location location);
   }
 }
