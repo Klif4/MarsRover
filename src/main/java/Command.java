a2211148e73c97828bdde20d32d90b87167efb08
klif <klif.cheloul@gmail.com>
2021-04-13 12:56:38 +0200

    wip

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -4,22 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 
 public enum Command {
-  FORWARD("f", MovingFunctions.forward),
-  BACKWARD("b", MovingFunctions.backward),
-  LEFT("l", MovingFunctions.left),
-  RIGHT("r", MovingFunctions.right);
+  FORWARD("f", ((direction, location) -> new CommandResponse(direction, direction.forward().apply(location)))),
+  BACKWARD("b", ((direction, location) -> new CommandResponse(direction, direction.backward().apply(location)))),
+  LEFT("l", ((direction, location) -> new CommandResponse(direction.turnLeft(), location))),
+  RIGHT("r", ((direction, location) -> new CommandResponse(direction.turnRight(), location)));
 
   private final String code;
-
-  public BiFunction<Direction, Location, CommandResponse> movingFunction() {
-    return movingFunction;
-  }
 
   private final BiFunction<Direction, Location, CommandResponse> movingFunction;
 
   Command(String code, BiFunction<Direction, Location, CommandResponse> movingFunction) {
     this.code = code;
     this.movingFunction = movingFunction;
+  }
+
+  public BiFunction<Direction, Location, CommandResponse> movingFunction() {
+    return movingFunction;
   }
 
   public static Command getByCode(String code) {
@@ -34,12 +34,5 @@ public enum Command {
   public static class CommandResponse {
     public Direction direction;
     public Location location;
-  }
-
-  private static class MovingFunctions {
-    private static final BiFunction<Direction, Location, CommandResponse> forward = ((direction, location) -> new CommandResponse(direction, direction.forward().apply(location)));
-    private static final BiFunction<Direction, Location, CommandResponse> backward = ((direction, location) -> new CommandResponse(direction, direction.backward().apply(location)));
-    private static final BiFunction<Direction, Location, CommandResponse> left = ((direction, location) -> new CommandResponse(direction.turnLeft(), location));
-    private static final BiFunction<Direction, Location, CommandResponse> right = ((direction, location) -> new CommandResponse(direction.turnRight(), location));
   }
 }
